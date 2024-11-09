208741dc423451e621c9ff16c54345232ad2724f
Ayishaakhan786 <ayeshakhan228276@gmail.com>
2024-11-09 08:48:50 +0500

    Updated repository with new changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+// eslint-disable react/no-unescaped-entities
 export default function About() {
     return (
       <h1 className="box text-blue-600 ml-8 mt-4 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
